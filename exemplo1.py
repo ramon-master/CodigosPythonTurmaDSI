7aaf2854ed7af7414779296133df790ee671e2b8
Ramon Emanuel Torres Penha <ramon.torres579@gmail.com>
2021-01-27 14:53:38 -0300

    Criado arquivo iniciar em phyton

---
+++ exemplo1.py
@@ -0,0 +1,4 @@
+nome = imput("Qual o seu nome?")
+end = input("Qual seu endereço?")
+idade = input("Qual sua idade?")
+printf("Ola´seu nome é ",nome, " voçe mora no endereço: ",end,"e tem", idade, "anos\n\n")
