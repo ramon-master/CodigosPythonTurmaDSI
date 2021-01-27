0cb31e4c5b1634a6ff7000bffc0d7717498ebd5f
Ramon Emanuel Torres Penha <ramon.torres579@gmail.com>
2021-01-27 15:46:44 -0300

    Criado arquivo exemplo 2

--- exemplo1.py
+++ exemplo1.py
@@ -1,4 +1,4 @@
-nome = imput("Qual o seu nome?")
+nome = input("Qual o seu nome?")
 end = input("Qual seu endereço?")
 idade = input("Qual sua idade?")
-printf("Ola´seu nome é ",nome, " voçe mora no endereço: ",end,"e tem", idade, "anos\n\n")
+print("Ola´seu nome é ",nome, " voçe mora no endereço: ",end,"e tem", idade, "anos\n\n")Ramon
---
+++ exemplo2.py
@@ -0,0 +1,17 @@
+# operadores + - / %
+# operador exponenciaçao **
+'''
+Será comentário de várias linhas
+
+
+num1 = 10
+num2 = 5
+print(num1 + num2)
+
+print(2**num1)
+'''
+nota1 = float(input("Informe a nota 1: "))
+nota2 = float(input("Informe a nota 2: "))
+média = (nota1 + nota2)/2
+
+print("A méida entre as notas {} e {} é {}".format(nota1, nota2, media))
